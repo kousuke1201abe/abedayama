31fe0ebfa166ae189cb5c1c9e37908930bd30707
kousuke1201abe <k-abe@giftee.co>
2019-07-15 00:20:03 +0900

    add anotation

--- app/models/quiz/question.rb
+++ app/models/quiz/question.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: quiz_questions
+#
+#  id         :bigint(8)        not null, primary key
+#  content    :string(255)      not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  quiz_id    :bigint(8)        not null
+#
+# Indexes
+#
+#  idx_contents_1        (quiz_id)
+#  idx_quiz_questions_2  (content)
+#
+# Foreign Keys
+#
+#  fk_quiz_questions_1  (quiz_id => quizzes.id)
+#
+
 class Quiz::Question < ApplicationRecord
   belongs_to :quiz
   has_many :incorrect_answers,
--- spec/factories/quiz_question_incorrect_answers.rb
+++ spec/factories/quiz_question_incorrect_answers.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: quiz_question_incorrect_answers
+#
+#  id               :bigint(8)        not null, primary key
+#  content          :string(255)      not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  quiz_question_id :bigint(8)        not null
+#
+# Indexes
+#
+#  idx_quiz_question_incorrect_answers_1  (quiz_question_id)
+#  idx_quiz_question_incorrect_answers_2  (content)
+#
+# Foreign Keys
+#
+#  fk_quiz_question_incorrect_answers_1  (quiz_question_id => quiz_questions.id)
+#
+
 FactoryBot.define do
   factory :quiz_question_incorrect_answer do
     sequence(:content) { |n| "test incorrect answer#{n}" }
