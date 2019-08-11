ba2de2b7121d28b10c75bd12d06ef4fc30a74de7
kousuke1201abe <k-abe@giftee.co>
2019-08-11 19:54:10 +0900

    yarn add vue-apollo graphql apollo-client apollo-link apollo-link-http apollo-link-context apollo-cache-inmemory graphql-tag vue-moment

--- frontend/src/root/index.js
+++ frontend/src/root/index.js
@@ -3,12 +3,16 @@ import Vuetify from 'vuetify';
 import 'vuetify/dist/vuetify.min.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
 import '@mdi/font/css/materialdesignicons.css'
+import { createProvider } from 'vue-apollo'
 import HelloWorld from './components/HelloWorld.vue';
+
+Vue.config.productionTip = false
 
 Vue.use(Vuetify)
 
 document.addEventListener('DOMContentLoaded', () => {
   new Vue({
+    apolloProvider: createProvider(),
     el: '#root',
     components: { HelloWorld },
     template: '<hello-world></hello-world>'
