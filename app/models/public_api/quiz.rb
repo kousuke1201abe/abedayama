42c28e9f8d2b9854eff06ce4b19bc560826e7731
kousuke1201abe <k-abe@giftee.co>
2019-08-12 20:27:34 +0900

    add mutation for creating quiz(only quiz name)

--- app/models/public_api/quiz.rb
+++ app/models/public_api/quiz.rb
@@ -1,18 +1,46 @@
 require 'itunes_search_api'
 
 class PublicAPI::Quiz < ApplicationModel
+  attribute :name, :string
+
+  validates :name, presence: true
+
   def self.all
     ::Quiz.all
   end
 
-  def self.fetch_songs_from_itunes_api(artist)
+  def self.create!(args)
+    song_list = fetch_songs_from_itunes_api(args[:name])
+    new(name: song_list.first["artistName"]).tap(&:save!)
+  end
+
+  def save!
+    validate!
+    internal_quiz.save!
+  end
+
+  def quiz
+    validate!
+    internal_quiz
+  end
+
+  private
+
+  def self.fetch_songs_from_itunes_api(artist_name)
     ITunesSearchAPI.search(
-        :term    => artist,
+        :term    => artist_name,
         :country => "jp",
         :media   => 'music',
         :lang    => 'ja_jp',
         :attribute => "artistTerm",
         :limit  => '200'
         )
+  end
+
+  def internal_quiz
+    @quiz ||=
+      ::Quiz.find_or_initialize_by(
+        name: name
+      )
   end
 end
