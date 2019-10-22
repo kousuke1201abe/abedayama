ee5c9391bdfcbcbbe4fc1b76db7a975d24c2ab9b
kousuke1201abe <k-abe@giftee.co>
2019-10-22 23:29:02 +0900

    fix public path

--- frontend/webpack.config.babel.js
+++ frontend/webpack.config.babel.js
@@ -91,7 +91,7 @@ export default {
   output: {
     path: path.join(__dirname, '..', 'public/webpack'),
     filename: '[name]-bundle-[hash].js',
-    publicPath: 'https://musiq-quiz-generator.herokuapp.com/'
+    publicPath: '/'
   },
 
   resolve: {
